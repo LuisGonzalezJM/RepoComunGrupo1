3b916960301553c41f9c45010bbc153e4e48c4d0
Igor (Clase) <igor.catavi.1@iesjulianmarias.es>
2025-02-17 13:22:19 +0100

    commit Igor

--- FicheroBase1.java
+++ FicheroBase1.java
@@ -1,8 +1,10 @@
 //Este es el fichero original que viene creado
 //1.- Línea original de Luis
-
-public class FicheroBase1{
+//commit Igor
+public class FicheroBase1 {
     public static void main(String[] args) {
         System.out.println("FicheroBase1");
     }
 }
+
+
